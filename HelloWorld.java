abb5e2d8f6147d4345fdac6941385726dd62d99f
sanjeev5645 <sanjeev5645@gmail.com>
2023-09-25 15:53:05 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
 public static void main(String[] args) {
 
-System.out.println("Hello, JI");
+System.out.println("Hello, Bhai");
 
 System.out.println("Hello");
 System.out.println("Yo");
